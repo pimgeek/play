9e9f693e29d710ea9a4ffe0a9055dc563332ca8f
pimgeek <wudi.biz@gmail.com>
2018-06-01 15:23:21 +0800

    1 增加少量注释 2 增加获取子卡片盒列表的方法 3 增加为指定卡片盒生成 Markdown 文档的方法（无递归处理） 4 增加为 Markdown 标题降级的方法 5 增加为指定卡片盒生成 Markdown 文档的方法（有递归处理）

--- youminds/pafx2markdown.py
+++ youminds/pafx2markdown.py
@@ -3,8 +3,9 @@
 import sys
 import xml.etree.ElementTree as xmlET
 
+# 获取当前卡片盒的所有属性
 def get_cardbox_all_attrs(cardbox):
-    props_dict = {}
+    props_dict = { 'description': '' }
     props = cardbox.find('properties')
     if props != None:
         for prop in props:
@@ -16,12 +17,13 @@ def get_cardbox_all_attrs(cardbox):
                 pass
     else:
         pass
-    return props_dict  
+    return props_dict
+
+# 获取当前卡片盒的备注信息（若有嵌入图片，也要导出）
 def get_cardbox_content(cardbox):
-    content_dict = {}
+    content_dict = { 'text': '' }
     content = cardbox.find('content')
     if content != None:
-        print(content.get('symbolname'))
         if content.get('contentrepresentation') == 'text' and \
             content.get('styledtext') == 'true':
             content_dict['text'] = content.find('text').text
@@ -32,10 +34,69 @@ def get_cardbox_content(cardbox):
         elif content.get('contentrepresentation') == 'stream':
             content_dict['stream'] = content.text
     return content_dict
+
+# 获取当前卡片盒的子卡片盒列表
 def get_sub_cardbox_list(cardbox):
-    return
+    if cardbox == None or \
+        cardbox == False or \
+        cardbox.find('childs') == None:
+        return []
+    else:
+        return cardbox.find('childs').getchildren()
+
+# 按指定的层次，递归地获取卡片盒中的内容
 def pafx2md_by_level(cardbox, level):
-    return
+    md_list = []
+    sub_md_list = []
+    if (cardbox == None or cardbox == False):
+        return []
+    elif (level < 0 or level > 5):
+        print("level 数值超出范围（必须是小于 6 的正整数）...")
+        return []
+    else:
+        cardbox_attr_dict = get_cardbox_all_attrs(cardbox)
+        cardbox_content_dict = get_cardbox_content(cardbox)
+        md_list.append("# " + cardbox_attr_dict['name'])
+        if cardbox_attr_dict['description'] != '' and \
+            cardbox_content_dict['text'] != '':
+            md_list.append(cardbox_attr_dict['description'] + "\n\n" + cardbox_content_dict['text'] + "\n\n----\n")
+        
+        sub_cardbox_list = get_sub_cardbox_list(cardbox)
+        if (level == 0 or len(sub_cardbox_list) == 0):
+            pass
+        else:
+            for sub_cardbox in sub_cardbox_list:
+                sub_md_list += pafx2md_by_level(sub_cardbox, level - 1)
+    md_list += list(map(markdown_indent, sub_md_list))
+    return md_list
+
+def export_cardbox_in_ascii(cardbox):
+    ascii_str = ''
+    if cardbox != None:
+        cardbox_attr_dict = get_cardbox_all_attrs(cardbox)
+        cardbox_content_dict = get_cardbox_content(cardbox)
+        ascii_str += '  ┌────────────────────────────────────\n'
+        if 'name' in cardbox_attr_dict.keys():
+            ascii_str += "  |卡片盒标题：%s\n" % cardbox_attr_dict['name']
+        if 'description' in cardbox_attr_dict.keys():
+            ascii_str += "  |卡片盒简介：%s\n" % cardbox_attr_dict['description']
+        if 'text' in cardbox_attr_dict.keys():
+            ascii_str += "  |卡片盒备注：%s\n" % cardbox_attr_dict['text']
+        if 'styledtext' in cardbox_attr_dict.keys():
+            ascii_str += "  |卡片盒备注（富文本）：%s\n" % cardbox_attr_dict['styledtext'][-10:]
+        if 'stream' in cardbox_content_dict.keys():
+            ascii_str += "  |卡片盒内嵌图片：%s\n" % cardbox_content_dict['stream'][-10:]
+        ascii_str += '  └──────────────────────────────────── \n'
+    else:
+        pass
+    return ascii_str
+    
+def markdown_indent(md_str):
+    if md_str != None and md_str.startswith('#'):
+        md_str = '#' + md_str;
+    else:
+        pass
+    return md_str
 
 # begin main script
 
@@ -44,13 +105,8 @@ files = sys.argv[1:]
 for file in files:
     pafx_obj = xmlET.parse(file).getroot()
     root_cardbox = pafx_obj[0][0]
-    for cardbox in root_cardbox.findall('childs')[0].getchildren():
-        print("======== %s ========" % cardbox.get('id'))
-        # cardbox_attr_dict = get_cardbox_all_attrs(cardbox)
-        cardbox_content_dict = get_cardbox_content(cardbox)
-        if 'text' in cardbox_content_dict.keys():
-            print(cardbox_content_dict['text'])
-        if 'styledtext' in cardbox_content_dict.keys():
-            print(cardbox_content_dict['styledtext'][-5:])
-        if 'stream' in cardbox_content_dict.keys():
-            print(cardbox_content_dict['stream'][-5:])
+    # print('\n╔══════════ %s' % file)
+    # for cardbox in get_sub_cardbox_list(root_cardbox):
+    #     print(export_cardbox_in_ascii(cardbox))
+    # print('\n╚══════════════════════════════')
+    print(str.join('\n', pafx2md_by_level(root_cardbox, 3)))
